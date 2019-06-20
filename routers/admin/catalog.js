04406f679a313098a19ad9e462b18e0c5b3dc188
hjzhi <hjzhi@cndatacom.com>
2019-06-20 13:16:47 +0800

    】

--- routers/admin/catalog.js
+++
@@ -1,9 +0,0 @@
-const tablelib=require('../../libs/table');
-
-module.exports=tablelib(
-  [
-    {title: '标题', name: 'title', type: 'text'},
-  ],
-  'catalog_table',
-  'catalog'
-);
--- routers/api/index.js
+++
@@ -1,5 +0,0 @@
-const Router=require('koa-router');
-
-let router=new Router();
-
-module.exports=router.routes();
