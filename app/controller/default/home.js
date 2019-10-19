0895a9abdd03ea6bde42d734394b99e8980f7867
jasonzhi123 <1244653559@qq.com>
2019-10-19 15:31:34 +0800

    【添加首页文章列表接口】

--- app/controller/default/home.js
+++ app/controller/default/home.js
@@ -3,11 +3,16 @@
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
-
-  async index() {
-    const result = await this.app.mysql.get('blog_content', {});
-    console.log(result);
-    this.ctx.body = result + '234fasd';
+  async getArticleList() {
+    const sql = 'SELECT article.id as id,' +
+      'article.title as title,' +
+      'article.introduce as introduce,' +
+      'article.addTime as addTime,' +
+      'article.view_count as view_count,' +
+      'type.typeName as typeName ' +
+      'FROM article LEFT JOIN type ON article.type_id = type.id';
+    const result = await this.app.mysql.query(sql);
+    this.ctx.body = { data: result };
   }
 }
 
